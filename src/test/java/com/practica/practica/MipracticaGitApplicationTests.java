f0329719adeff7033faa1d6dca48db384dd1d228
Fernando Moutinho <moutinho132@gmail.com>
2024-01-25 10:49:43 -0400

    Mi primer commit

---
+++ src/test/java/com/practica/practica/MipracticaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MipracticaGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
